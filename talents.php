e683cce4332ad03e84878d339891342b36ea7057
¨Alicia <Alicia.Rodriguez.benaches@student.nhlstenden.com>
2022-12-11 20:24:12 +0100

    general structure and css file uploaded

--- talents.php
+++ talents.php
@@ -1,3 +1,39 @@
-<?php
-echo "test";
-?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Talents</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+
+    <body>
+        <div id="container">
+            <div id="talent1">
+                <img class="picture" alt="talent picture">
+                <div class="information">
+                    <div class="description">
+
+                    </div>
+
+                </div>
+            </div>
+            <div id="talent2">
+                <div class="information">
+                    <div class="description">
+
+                    </div>
+                </div>
+                <img class="picture" alt="talent picture">
+            </div>
+            <div id="talent3">
+                <img class="picture" alt="talent picture">
+                <div class="information">
+                    <div class="description">
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
