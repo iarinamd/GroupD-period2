ead24b81050ccf96b3ed05dede907972b88571d8
Alex Schuepbach <alex.schuepbach@student.nhlstenden.com>
2022-12-11 19:01:52 +0100

    Create home-page.php and page structure

---
+++ home-page.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Home page</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="home-page-style.css";
+    </head>
+
+    <body>
+        <div id="container">
+            <main>
+
+                <h3>HOT EVENT</h3>
+                <h1>Annual Emmen Music Festival 2023</h1>
+                <p><b>May 20th, 2023</b></p>
+                <p>Radhuisplein</p>
+                <p>7811 DC, Emmen</p>
+
+                <a href="">FIND OUT MORE</a>
+
+            </main>
+
+            <aside>
+                <h4>UPCOMING EVENTS</h4>
+
+                <div class="event">
+                    <p>Date</p>
+                    <h2>EVENT 1</h2>
+                    <a href="">More Info</a>
+                </div>
+
+                <div class="event">
+                    <p>Date</p>
+                    <h2>EVENT 2</h2>
+                    <a href="">More Info</a>
+                </div>
+
+                <div class="event">
+                    <p>Date</p>
+                    <h2>EVENT 3</h2>
+                    <a href="">More Info</a>
+                </div>
+            </aside>
+        </div>
+    </body>
+</html>
