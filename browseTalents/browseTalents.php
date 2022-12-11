4ee748ef4f5bc6dc909caba7fea2191f675b59c8
izabelarybinska <izabela.rybinska@student.nhlstenden.com>
2022-12-11 11:48:13 +0100

    Temp header added

--- browseTalents/browseTalents.php
+++ browseTalents/browseTalents.php
@@ -1,3 +1,24 @@
 <?php
-    echo "test 2"
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Linking CSS and meta tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Browse Talents</title>
+</head>
+<div id="mainContainer">
+    <header>
+        <!-- Paste Iarina's Header Code
+        For now using color blocks for header-->
+        <h1>Placeholder for header</h1>
+    </header>
+    <main>
+
+    </main>
+</div>
+</html>
