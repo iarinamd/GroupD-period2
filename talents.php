0d82968af3966045de2a3a0d0c5d740e5eaabeb9
¨Alicia <Alicia.Rodriguez.benaches@student.nhlstenden.com>
2022-12-12 20:56:49 +0100

    edit description of talents

--- talents.php
+++ talents.php
@@ -10,25 +10,46 @@
         <div id="container">
             <div id="talent1">
                 <img class="picture1" src="images/b_3.jpg" alt="talent picture">
-                <div class="information">
-                    <div class="description">
+                <div class="information1">
+                    <div class="column1">
+
+                    </div>
+                    <div class="description1">
 
                     </div>
 
                 </div>
             </div>
             <div id="talent2">
-                <div class="information">
-                    <div class="description">
-
+                <div class="information2">
+                    <div class="column21">
+                        <h1>Talent's name</h1>
+                        <p class="description">Nam tincidunt ipsum ac placerat ornare. Donec hendrerit suscipit volutpat.
+                            Nulla ut lacus sem. Vestibulum sit amet congue sem. Nulla facilisi. Donec a urna id justo
+                            consequat vehicula. Proin faucibus rutrum est, at sagittis lorem fringilla quis.
+                            Proin tristique posuere tellus eget tincidunt. Etiam egestas condimentum tempor.</p>
                     </div>
+                    <div class="column22">
+                        <p>-></p>
+                        <p>-></p>
+                        <p>-></p>
+                    </div>
+                    <div class="column23">
+                        <p>Specialty 1</p>
+                        <p>Specialty 2</p>
+                        <p>Specialty 3</p>
+                        <div class="button">
+                            <p>Find out more</p>
+                        </div>
+                    </div>
+
                 </div>
                 <img class="picture2" src="images/a_1.jpg" alt="talent picture">
             </div>
             <div id="talent3">
                 <img class="picture3" src="images/c_1.jpg" alt="talent picture">
-                <div class="information">
-                    <div class="description">
+                <div class="information3">
+                    <div class="description3">
 
                     </div>
 
