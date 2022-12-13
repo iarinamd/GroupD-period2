0aa2c977c99c130cad4701b882f058ef50b5e0f3
AlexMednov <alex.mednov@student.nhlstenden.com>
2022-12-13 17:07:58 +0100

    Establishing connections and arrainging the layout

---
+++ index_ev1.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href = "style_ev1.css" type = "text/css" rel = "stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <div id = "grid_container">
+        <footer>
+            Header
+            <nav>
+                nav
+            </nav>
+        </footer>
+        <div id = "main_body">
+            main body
+
+            picture
+        </div>
+
+        <footer>
+            footer
+        </footer>
+    </div>
+</body>
+</html>
