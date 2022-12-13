81da37c89084f6d96a5431227829d1bb7743ed6b
izabelarybinska <izabela.rybinska@student.nhlstenden.com>
2022-12-13 16:09:50 +0100

    Finished browseTalents page

--- browseTalents/browseTalents.php
+++ browseTalents/browseTalents.php
@@ -8,10 +8,10 @@
     <!-- Linking CSS and meta tags -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/style1.2.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>Browse Talents</title>
 </head>
-<div id="mainContainer">
+<div class="mainContainer">
     <header>
         <!-- Paste Iarina's header code
         For now using color block for header-->
@@ -20,19 +20,15 @@
     <!-- Start of talent categories section
          "cat" = "categories"-->
     <main>
-        <div id="catContainer">
-            <div id="musicCat">
-                <h1>Concerts</h1>
-                <!--<img src="img/event_images/concerts/concert_5.jpg"
-                     id="imgMusicCat"
-                     alt="Drum set">-->
-
+        <div class="catContainer">
+            <div class="musicCat">
+                <h1 class="concerts"><a href="#">Concerts</a></h1>
             </div>
-            <div id="partiesCat">
-                <h1>Parties</h1>
+            <div class="partiesCat">
+                <h1 class="parties"><a href="#">Parties</a></h1>
             </div>
-            <div id="othersCat">
-                <h1>Others</h1>
+            <div class="othersCat">
+                <h1 class="others"><a href="#">Others</a></h1>
             </div>
 
         </div>
