43ff997cd4e83e771cce51637378ae6b3b7dca04
izabelarybinska <izabela.rybinska@student.nhlstenden.com>
2022-12-13 16:47:16 +0100

    html browseCategories added

---
+++ browseCategories/browseCategories.php
@@ -0,0 +1,38 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Linking CSS and meta tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Browse Categories</title>
+</head>
+<div class="mainContainer">
+    <header>
+        <!-- Paste Iarina's header code
+        For now using color block for header-->
+        <h1>Placeholder for header</h1>
+    </header>
+    <!-- Start of talent categories section
+         "cat" = "categories"-->
+    <main>
+        <div class="catContainer">
+            <div class="musicCat">
+                <h1 class="concerts"><a href="#">Concerts</a></h1>
+            </div>
+            <div class="partiesCat">
+                <h1 class="parties"><a href="#">Parties</a></h1>
+            </div>
+            <div class="othersCat">
+                <h1 class="others"><a href="#">Others</a></h1>
+            </div>
+
+        </div>
+
+    </main>
+</div>
+</html>
