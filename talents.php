c0916d4fa65cd90bc0f182a8d0830fa917951b2d
¨Alicia <Alicia.Rodriguez.benaches@student.nhlstenden.com>
2022-12-13 16:24:56 +0100

    css folder created

--- talents.php
+++ talents.php
@@ -24,25 +24,26 @@
                 <div class="information2">
                     <div class="column21">
                         <h1>Talent's name</h1>
-                        <p class="description">Nam tincidunt ipsum ac placerat ornare. Donec hendrerit suscipit volutpat.
-                            Nulla ut lacus sem. Vestibulum sit amet congue sem. Nulla facilisi. Donec a urna id justo
-                            consequat vehicula. Proin faucibus rutrum est, at sagittis lorem fringilla quis.
-                            Proin tristique posuere tellus eget tincidunt. Etiam egestas condimentum tempor.</p>
+                        <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a lacinia
+                            leo, sed blandit felis. Sed ultricies consectetur quam quis efficitur. Etiam id magna id
+                            felis suscipit aliquet. Ut eros enim, interdum non ipsum id, suscipit congue ex. Duis laoreet,
+                            lorem quis egestas imperdiet, urna justo pharetra ex, ut laoreet nisl nibh sed neque.
+                            Maecenas eget mi quam. Nunc molestie aliquet nisl a vestibulum. Morbi pellentesque, libero
+                            id fermentum elementum, tellus nisl aliquam nisl, sed commodo purus nunc et massa. Vestibulum
+                            ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vestibulum ante
+                            ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Phasellus dignissim
+                            aliquet mauris, quis pellentesque justo semper sed.</p>
                     </div>
                     <div class="column22">
-                        <p>-></p>
-                        <p>-></p>
-                        <p>-></p>
-                    </div>
-                    <div class="column23">
-                        <p>Specialty 1</p>
-                        <p>Specialty 2</p>
-                        <p>Specialty 3</p>
+                        <div class="specialty">
+                            <p>-> Specialty1</p>
+                            <p>-> Specialty2</p>
+                            <p>-> Specialty3</p>
+                        </div>
                         <div class="button">
                             <p>Find out more</p>
                         </div>
                     </div>
-
                 </div>
                 <img class="picture2" src="images/a_1.jpg" alt="talent picture">
             </div>
