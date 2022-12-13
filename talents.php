2cb8af708014a802106b2da5f6a355795260bfb1
¨Alicia <Alicia.Rodriguez.benaches@student.nhlstenden.com>
2022-12-13 16:31:54 +0100

    description of talents finished

--- talents.php
+++ talents.php
@@ -3,7 +3,7 @@
     <head>
         <title>Talents</title>
         <meta charset="UTF-8">
-        <link rel="stylesheet" type="text/css" href="style.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
     </head>
 
     <body>
@@ -11,11 +11,27 @@
             <div id="talent1">
                 <img class="picture1" src="images/b_3.jpg" alt="talent picture">
                 <div class="information1">
-                    <div class="column1">
-
+                    <div class="column21">
+                        <h1>Talent's name</h1>
+                        <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a lacinia
+                            leo, sed blandit felis. Sed ultricies consectetur quam quis efficitur. Etiam id magna id
+                            felis suscipit aliquet. Ut eros enim, interdum non ipsum id, suscipit congue ex. Duis laoreet,
+                            lorem quis egestas imperdiet, urna justo pharetra ex, ut laoreet nisl nibh sed neque.
+                            Maecenas eget mi quam. Nunc molestie aliquet nisl a vestibulum. Morbi pellentesque, libero
+                            id fermentum elementum, tellus nisl aliquam nisl, sed commodo purus nunc et massa. Vestibulum
+                            ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vestibulum ante
+                            ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Phasellus dignissim
+                            aliquet mauris, quis pellentesque justo semper sed.</p>
                     </div>
-                    <div class="description1">
-
+                    <div class="column22">
+                        <div class="specialty">
+                            <p>-> Specialty1</p>
+                            <p>-> Specialty2</p>
+                            <p>-> Specialty3</p>
+                        </div>
+                        <div class="button">
+                            <p>Find out more</p>
+                        </div>
                     </div>
 
                 </div>
@@ -49,12 +65,29 @@
             </div>
             <div id="talent3">
                 <img class="picture3" src="images/c_1.jpg" alt="talent picture">
-                <div class="information3">
-                    <div class="description3">
-
+                <div class="information1">
+                    <div class="column21">
+                        <h1>Talent's name</h1>
+                        <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a lacinia
+                            leo, sed blandit felis. Sed ultricies consectetur quam quis efficitur. Etiam id magna id
+                            felis suscipit aliquet. Ut eros enim, interdum non ipsum id, suscipit congue ex. Duis laoreet,
+                            lorem quis egestas imperdiet, urna justo pharetra ex, ut laoreet nisl nibh sed neque.
+                            Maecenas eget mi quam. Nunc molestie aliquet nisl a vestibulum. Morbi pellentesque, libero
+                            id fermentum elementum, tellus nisl aliquam nisl, sed commodo purus nunc et massa. Vestibulum
+                            ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Vestibulum ante
+                            ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Phasellus dignissim
+                            aliquet mauris, quis pellentesque justo semper sed.</p>
                     </div>
-
-                </div>
+                    <div class="column22">
+                        <div class="specialty">
+                            <p>-> Specialty1</p>
+                            <p>-> Specialty2</p>
+                            <p>-> Specialty3</p>
+                        </div>
+                        <div class="button">
+                            <p>Find out more</p>
+                        </div>
+                    </div>
             </div>
         </div>
     </body>
