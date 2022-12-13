4f6cca8a7989495d04f9f0e8d701b2417b01bbd3
AlexMednov <alex.mednov@student.nhlstenden.com>
2022-12-13 21:04:08 +0100

    Made main body with some placeholder images

---
+++ indexEv1.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href = "styleEv1.css" type = "text/css" rel = "stylesheet">
+    <title>Event</title>
+</head>
+<body>
+    <div id = "gridContainer">
+        <header>
+            <img src="img/e3tLogo.png" alt="E3T_logo">
+            <nav>
+                <ul>
+                    <li><a href="#">BROWSE TALENTS</a></li>
+                    <li><a href="#">EVENTS</a></li>
+                    <li><a href = "#">LOGIN</a></li>
+                </ul>
+            </nav>
+        </header>
+        <div id = "mainBody">
+            <div id = "eventInf">
+                <h1>Place holder event name</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Purus viverra accumsan in nisl nisi scelerisque eu ultrices vitae. Ligula ullamcorper malesuada proin libero nunc consequat interdum. </p>
+                <div id="shapes">
+                    <div class="shapesTextBox">
+                        <img src="img/placeShape.png" alt="Place icon" class="shapes">
+                        <a href="#" class="anchor">Location</a>
+                    </div>
+                    <div class="shapesTextBox">
+                        <img src="img/timeShape.png" alt="Time icon" class="shapes">
+                        <a href="#" class="anchor">Time</a>
+                    </div>
+                    <div class="shapesTextBox">
+                        <img src="img/capacityShape.png" alt="Capacity icon" class="shapes">
+                        <a href="#" class="anchor">Capacity</a>
+                    </div>
+                </div>
+            </div>
+            <img src="img/placeHolder.jpg" alt="PlaceHolder" id = "eventPicture">
+        </div>
+
+        <footer>
+            footer
+        </footer>
+    </div>
+</body>
+</html>
