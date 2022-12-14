d13218a945f2755677955f2fa864377ca2d9a632
Bivol Maksym <maksym.bivol@student.nhlstenden.com>
2022-12-14 14:16:31 +0100

    css + img fix

--- li-profile-edit.php
+++ li-profile-edit.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <title>Profile</title>
-  <link rel="stylesheet" type="text/css" href="li-profile-edit.css">
+  <link rel="stylesheet" type="text/css" href="css/li-profile-edit.css">
 </head>
 <body>
 <div id="maincontainer">
