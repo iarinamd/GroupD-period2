48e9cd215f308857f0792ae3177c69377b66329f
izabelarybinska <izabela.rybinska@student.nhlstenden.com>
2022-12-19 14:44:51 +0100

    creating new clients page

--- newClient/newClient.php
+++ newClient/newClient.php
@@ -7,9 +7,9 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="css/style.css">
-        <title>Create a new Client</title>
+        <title>Create a new Client!</title>
     </head>
     <body>
-        
+
     </body>
 </html>
