64fec1b71257de1214e9db1d9981bea7569d20a4
izabelarybinska <izabela.rybinska@student.nhlstenden.com>
2022-12-19 14:42:22 +0100

    starting add clients page

---
+++ newClient/newClient.php
@@ -0,0 +1,15 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/style.css">
+        <title>Create a new Client</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
