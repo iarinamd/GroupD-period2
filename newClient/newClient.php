8394e3a402098218b29dd708a82f49c5a0e3e812
izabelarybinska <izabela.rybinska@student.nhlstenden.com>
2022-12-20 11:28:30 +0100

    added first lines of form

--- newClient/newClient.php
+++ newClient/newClient.php
@@ -10,6 +10,35 @@
         <title>Create a new Client!</title>
     </head>
     <body>
-
+        <div id="container">
+            <div id="header">
+                <!-- PASTE IARINA'S HEADER HERE!-->
+            </div>
+            <div id="mainContainer">
+                <h1>Create a new user</h1>
+                <div id="form">
+                    <form action="processedForm.php" method="POST">
+                        <div id="formLabel">
+                            <label for="fName" id="fName">First Name</label>
+                            <label for="lName" id="lName">Last Name</label>
+                            <label for="specialty" id="specialty">Specialty</label>
+                        </div>
+                        <input type="Text" name="fName">
+                        <input type="Text" name="lName">
+                        <select name="specialty" autocomplete="off">
+                            <optgroup label="Music">
+                                <option value="Musician">Musician</option>
+                                <option value="DJs">DJs</option>
+                                <option value="Band">Band</option>
+                            </optgroup>
+                            <optgroup label="Party">
+                                <option value="Coordinators">Coordinators</option>
+                                <option value="Caterers">Caterers</option>
+                            </optgroup>
+                        </select>
+                    </form>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
