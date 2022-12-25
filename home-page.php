280a98bc12874bcb6c328e3e904d60c4211e5d7f
Alex Schuepbach <alex.schuepbach@student.nhlstenden.com>
2022-12-25 11:57:42 +0700

    Added temporary header and footer

--- home-page.php
+++ home-page.php
@@ -8,6 +8,22 @@
     </head>
 
     <body>
+
+        <header>
+            <div id ="headerImage">
+                <a href="#">
+                    <img class="logoImg" src="image/E3T_LOGO.png" alt="E3T logo">
+                </a>
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="#">BROWSE TALENTS</a></li>
+                    <li><a href="#">EVENTS</a></li>
+                    <li><a href="#">LOGIN</a></li>
+                </ul>
+            </nav>
+        </header>
+
         <div id="container">
             <main>
                 <div id="mainTitle">
@@ -63,5 +79,30 @@
                 <p></p>
             </aside>
         </div>
+
+        <footer>
+            <div class="contactInformations">
+                <h3> CONTACT</h3>
+                <span>+31 123456789 contact@e3t.com </span>
+                <span>+31 987654321 1111AA, Emmen</span>
+            </div>
+            <div class="openHours">
+                <h3>OPENING HOURS</h3>
+                <span>Monday - Saturday: 9:30 - 18:00</span>
+                <span>Sunday: Closed</span>
+            </div>
+            <div class="iconImg">
+                <a href="https://nl-nl.facebook.com/">
+                    <img class="facebookImg" src="image/facebook_logo.png" alt="facebook logo">
+                </a>
+                <a href="https://www.instagram.com/">
+                    <img class="instagramImg" src="image/instagram_logo.png" alt="instagram logo">
+                </a>
+                <a href="https://www.tiktok.com/login">
+                    <img class="tiktokImg" src="image/tiktok_logo.png" alt="tiktok logo">
+                </a>
+            </div>
+        </footer>
+    
     </body>
 </html>
