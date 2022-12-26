3f2c76475e294eabdb382da65f985d1324f7e278
¨Alicia <Alicia.Rodriguez.benaches@student.nhlstenden.com>
2022-12-26 17:12:41 +0100

    created php and css file

---
+++ booking-system/bookingSystem.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="eng">
+    <head>
+        <title>bookingSystem</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+    </head>
+</html>
