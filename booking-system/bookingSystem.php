864dfafc8b5ae57a83bdb66ec8bd9fb151eb9b3a
¨Alicia <Alicia.Rodriguez.benaches@student.nhlstenden.com>
2022-12-30 13:06:20 +0100

    added margins

--- booking-system/bookingSystem.php
+++ booking-system/bookingSystem.php
@@ -7,55 +7,52 @@
     </head>
 
     <body>
+    <!-- Header -->
         <h1>Book a talent</h1>
         <div id="container">
             <form name="booking" action="bookingSystem.php" method="POST" id="form">
                 <div class="row1">
                     <div class="talent">
-                        <label for="talent">Select a talent</label>
                         <select name="type" id="type">
+                            <option disabled selected>Select a talent</option>
                             <option>Talent1</option>
                             <option>Talent2</option>
                             <option>Talent3</option>
                         </select>
                     </div>
                     <div class="date">
-                        <label for="date">Date</label>
                         <input type="date" name="date" id="date">
                     </div>
                     <div class="time">
-                        <label for="time">Time</label>
-                        <input type="text" name="time" id="time">
+                        <input type="text" name="time" id="time" placeholder="Time">
                     </div>
                 </div>
 
                 <div class="row2">
                     <div class="category">
-                        <label for="category">Category</label>
                         <select name="type" id="type">
+                            <option diable selected>Category</option>
                             <option>Category1</option>
                             <option>Category2</option>
                             <option>Category3</option>
                         </select>
                     </div>
                     <div class="address">
-                        <label for="address">Address</label>
-                        <input type="text" name="address" id="address">
+                        <input type="text" name="address" id="address" placeholder="Address">
                     </div>
                     <div class="zipCode">
-                        <label for="zipCode">Zip Code</label>
-                        <input type="text" name="zipCode" id="zipCode">
+                        <input type="text" name="zipCode" id="zipCode" placeholder="Zip Code">
                     </div>
                 </div>
                 <div class="description">
-                    <label for="description">Description</label>
-                    <texrtarea name="description"></texrtarea>
+                    <textarea placeholder="Description"></textarea>
                 </div>
                 <div class="uploadFile">
                     <label for="uploadFile">Upload files(optional)</label>
                     <input type="file" name="uploadedFile" id="uploadedFile">
                 </div>
             </form>
+            <!-- Footer -->
         </div>
 
     </body>
