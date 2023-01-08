e187e0b5c25d7429e899369b2e8057b775e63cb3
Iarina <112045565+iarinamd@users.noreply.github.com>
2023-01-08 21:12:07 +0100

    This is the php for the login page. Is fully working.

---
+++ login.php
@@ -0,0 +1,82 @@
+<?php
+session_start(); //
+//Processing Page for logging in
+if ($_SERVER['REQUEST_METHOD'] == 'POST') // related to form, verify if the method is POST
+{
+    //Here we are getting the input information from the forms.
+    $memberUsername = $_POST["userInput"];
+    $memberPassword = $_POST["passwordInput"]; // this is how you take the value from an input text box.
+
+    //Here we are trying to connect to the database.
+    if ($conn = mysqli_connect('localhost','root',''))
+    {
+        mysqli_select_db($conn, 'project_2'); // change the database name at the end!!
+
+        //this is prepare statement for security
+        $sql = "SELECT `username`, `password`, `usertype` FROM `login` WHERE `username`= ?";
+        if ($stmt = mysqli_prepare($conn, $sql))
+        {
+            mysqli_stmt_bind_param($stmt, 's', $memberUsername);
+
+            //Here we are executing the statement(the "$sql")
+            if (mysqli_stmt_execute($stmt))
+            {
+
+            }
+            else
+            {
+                echo "Error. Failed to check staff ID. Try again later";
+            }
+        }
+        else
+        {
+            echo "Preparation error. Try again later";
+        }
+
+        //Bind results
+        mysqli_stmt_bind_result($stmt, $userName, $password, $userType);
+        mysqli_stmt_store_result($stmt); // verify if the values from data base are stored in values from above
+        if (mysqli_stmt_num_rows($stmt) > 0) // We are checking if the database in empty or not
+        {
+            mysqli_stmt_fetch($stmt);
+            if ($memberUsername == $userName AND $memberPassword== $password  )
+            { // We are checkin the username and the password of a user.
+                switch ($userType)
+                {
+                    case "admin":
+                        echo '<script type="text/javascript">location.href = "#";</script>';
+                        break;
+                    case "user":
+                        echo '<script type="text/javascript">location.href = "#";</script>';
+                    break;
+                    case "talent":
+                        echo '<script type="text/javascript">location.href = "#";</script>';
+                    break;
+                    default:
+                        echo "<script type='text/javascript'>alert('Something went wrong.Please try again!');</script>";
+                        echo '<script type="text/javascript">location.href = "login.html";</script>';
+                }
+            }
+            else
+            {
+                //This is happening if the username and password do not match
+                session_destroy();
+                echo "<script type='text/javascript'>alert('Incorrect username or password.Please try again');</script>";
+                echo '<script type="text/javascript">location.href = "login.html";</script>';
+            }
+        }
+        else
+        {
+            //This is happening when the database is empty.
+            session_destroy();
+            echo "<script type='text/javascript'>alert('The username can not be found.Please try again!');</script>";
+            echo '<script type="text/javascript">location.href = "login.html";</script>';
+        }
+    }
+    else
+    {
+        echo "<script type='text/javascript'>alert('The connection can not be establish.Please try again later!');</script>";
+        echo '<script type="text/javascript">location.href = "login.html";</script>';
+    }
+}
+?>
