c0c30b18a973f7d3df00ace28d7c51843f089a7e
izabelarybinska <izabela.rybinska@student.nhlstenden.com>
2023-01-08 16:03:48 +0100

    finished html

--- newClient/newClient.php
+++ newClient/newClient.php
@@ -13,29 +13,43 @@
         <div id="container">
             <div id="header">
                 <!-- PASTE IARINA'S HEADER HERE!-->
+                <h1>Header Placeholder</h1>
             </div>
             <div id="mainContainer">
                 <h1>Create a new user</h1>
                 <div id="form">
-                    <form action="processedForm.php" method="POST">
-                        <div id="formLabel">
-                            <label for="fName" id="fName">First Name</label>
-                            <label for="lName" id="lName">Last Name</label>
-                            <label for="specialty" id="specialty">Specialty</label>
+                    <form action="processedForm.php" method="POST" enctype="multipart/form-data">
+                        <div id="firstRowInput">
+                            <input type="text" name="fName" id="fName" placeholder="First Name">
+                            <input type="text" name="lName" id="lName" placeholder="Last Name">
+
+                            <select name="specialty" autocomplete="off">
+                                <optgroup label="Music">
+                                    <option value="musician">Musician</option>
+                                    <option value="djs">DJs</option>
+                                    <option value="band">Band</option>
+                                </optgroup>
+                                <optgroup label="Party">
+                                    <option value="coordinators">Coordinators</option>
+                                    <option value="caterers">Caterers</option>
+                                </optgroup>
+                                <optgroup label="Other">
+                                    <option value="other">Other</option>
+                                </optgroup>
+                            </select>
+                            <p></p>
                         </div>
-                        <input type="Text" name="fName">
-                        <input type="Text" name="lName">
-                        <select name="specialty" autocomplete="off">
-                            <optgroup label="Music">
-                                <option value="Musician">Musician</option>
-                                <option value="DJs">DJs</option>
-                                <option value="Band">Band</option>
-                            </optgroup>
-                            <optgroup label="Party">
-                                <option value="Coordinators">Coordinators</option>
-                                <option value="Caterers">Caterers</option>
-                            </optgroup>
-                        </select>
+                        <div id="secondRowInput">
+                            <input type="email" name="email" id="email" placeholder="E-Mail">
+                            <input type="tel" name="phoneNr" id="phoneNr" placeholder="Phone Number">
+                            <input type="text" name="zip" id="zip" placeholder="ZIP Code">
+                        </div>
+                        <div id="thirdRowInput">
+                            <textarea>Write your description here</textarea>
+                            <input type="file" name="uploadedFile" id="uploadedFile">
+                            <p></p>
+                        </div>
+                        <input type="submit" value="Save Profile">
                     </form>
                 </div>
             </div>
