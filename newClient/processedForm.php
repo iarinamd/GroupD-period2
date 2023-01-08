08cc21febf7f9169a5cd121c513d489a18807583
izabelarybinska <izabela.rybinska@student.nhlstenden.com>
2023-01-08 17:22:43 +0100

    php form validation started

--- newClient/processedForm.php
+++ newClient/processedForm.php
@@ -1,3 +1,25 @@
 <?php
+    $fNameErr = $lNameErr = $specialtyErr = $emailErr = $phoneNrErr = $zipErr = $uploadedFileErr = " ";
+    $fName = $lName = $specialty = $email = $phoneNr = $zip = $uploadedFile = " ";
 
+    if ($_SERVER["REQUEST_METHOD"]=="POST"){
+        if (empty($_POST["fName"])){
+            $fNameErr= "Please enter a valid name";
+        } else{
+            $fName= test_input($_POST["fName"]);
+            if (preg_match("/^[a-zA-Z-' ]*$/",$fName)){
+                $fNameErr="Only letters and white spaces allowed";
+            }
+        }
+        if (empty($_POST["lName"])){
+            $lNameErr= "Please enter a valid name";
+        } else{
+            $lName= test_input($_POST["lName"]);
+            if (preg_match("/^[a-zA-Z-' ]*$/",$lName)){
+                $lNameErr="Only letters and white spaces allowed";
+            }
+        } if (empty($_POST["specialty"])){
+            $specialtyErr = "Please select a specialty.";
+        }
+    }
 ?>
