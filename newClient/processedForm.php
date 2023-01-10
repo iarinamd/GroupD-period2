05cf07a76ec0698199c8f0d3f2dec0968f32d696
izabelarybinska <izabela.rybinska@student.nhlstenden.com>
2023-01-10 16:10:37 +0100

    almost finished php with test uploads

--- newClient/processedForm.php
+++ newClient/processedForm.php
@@ -1,34 +1,113 @@
 <?php
-    $fNameErr = $lNameErr = $specialtyErr = $emailErr = $phoneNrErr = $zipErr = $uploadedFileErr = " ";
-    $fName = $lName = $specialty = $email = $phoneNr = $zip = $uploadedFile = " ";
+//FILE VALIDATION
 
-    if ($_SERVER["REQUEST_METHOD"]=="POST"){
-        if (empty($_POST["fName"])){
-            $fNameErr= "Please enter a valid name";
-        } else{
-            $fName= test_input($_POST["fName"]);
-            if (preg_match("/^[a-zA-Z-' ]*$/",$fName)){
-                $fNameErr="Only letters and white spaces allowed";
+$fileSize = (7*1024*1024);
+
+if ($_FILES["uploadedFile"]["error"] == 0) {
+    if (strlen($_FILES["uploadedFile"]["name"]) < 50 && preg_match('/[A-Z]/', $_FILES["uploadedFile"]["name"])) {
+        if ($_FILES["uploadedFile"]["size"] < $fileSize) {
+            $acceptedTypes = ["image/png", "image/jpeg", "image/jpg"];
+            $uploadedFileType = finfo_file(finfo_open(FILEINFO_MIME_TYPE), $_FILES["uploadedFile"]["tmp_name"]);
+
+            if (in_array($uploadedFileType, $acceptedTypes)) {
+                if (!file_exists("uploadFile/" . $_FILES["uploadedFile"]["name"])) {
+                    if(move_uploaded_file($_FILES["uploadedFile"]["tmp_name"], "uploadFile/". $_FILES["uploadedFile"]["name"])) {
+                        $uploadedFile = "uploadFile/". $_FILES["uploadedFile"]["name"];
+                        echo '<img src="'.$uploadedFile.'">';
+                        echo $uploadedFile;
+                        echo "Upload: ". $_FILES["uploadedFile"]["name"] ."<br>";
+                        echo "Type: ". $_FILES["uploadedFile"]["type"] ."<br>";
+                        echo "Size: ". ($_FILES["uploadedFile"]["size"] / 1024) ." Kb<br>";
+                        echo "Stored temporarily in: ". $_FILES["uploadedFile"]["tmp_name"] ."<br>";
+                        echo "Stored permanently in: " . "upload/". $_FILES["uploadedFile"]["name"];
+                    } else {
+                        echo "Something went wrong while uploading";
+                    }
+                } else {
+                    echo $_FILES["uploadedFile"]["name"] . " already exists";
+                }
+            } else {
+                echo "Invalid file type (" . $_FILES["uploadedFile"]["type"] . ")";
             }
+        } else {
+            echo "Invalid file size (" . $_FILES["uploadedFile"]["size"] . ")";
         }
-        if (empty($_POST["lName"])){
-            $lNameErr= "Please enter a valid name";
-        } else{
-            $lName= test_input($_POST["lName"]);
-            if (preg_match("/^[a-zA-Z-' ]*$/",$lName)){
-                $lNameErr="Only letters and white spaces allowed";
-            }
-        } if (empty($_POST["specialty"])){
-            $specialtyErr = "Please select a specialty.";
-        } else{
-            echo "Specialty selected";
-        }
-        if (empty($_POST["email"])){
-            $emailErr= "Valid E-Mail.";
-        } else{
-            $email= test_input($_POST["email"]);
-        } if (!filter_var($email,FILTER_VALIDATE_EMAIL)){
-            $emailErr = "The E-Mail address is not a correct format";
-        }
+    } else {
+        echo "Name must contain an uppercase and must not exceed 50 chars";
     }
+} else {
+    echo "Error: ". $_FILES["uploadedFile"]["error"] ."<br>";
+}
+
+if ($_SERVER["REQUEST_METHOD"]=="POST"){
+    $fName = filter_input(INPUT_POST, "fName");
+    $lName = filter_input(INPUT_POST, "lName");
+    $specialty1 = filter_input(INPUT_POST, "specialty1");
+    $specialty2 = filter_input(INPUT_POST, "specialty2");
+    $specialty3 = filter_input(INPUT_POST, "specialty3");
+    $email = filter_input(INPUT_POST, "email",FILTER_VALIDATE_EMAIL);
+    $phoneNr = filter_input(INPUT_POST, "phoneNr");
+    $bday = filter_input(INPUT_POST, "bday");
+    $description = filter_input(INPUT_POST, "description");
+
+    if(empty($fName)){
+        echo "Please enter your first name";
+    } elseif (empty($lName)){
+        echo "Please enter your last name";
+    } elseif (empty($specialty1)){
+        echo "Please select a specialty";
+    } elseif (empty($specialty2)){
+        echo "Please select a specialty";
+    }elseif (empty($specialty3)){
+        echo "Please select a specialty";
+    } elseif (empty($email)){
+        echo "Please enter an E-Mail address";
+    } elseif (empty($phoneNr)){
+        echo "Please enter a phone number";
+    } elseif (empty($bday)){
+        echo "Please enter your date of birth";
+    } else{
+        echo "Your profile has been saved with the following data:" . "<br>";
+        echo "First Name: ". $fName ."<br>";
+        echo "Last Name: ". $lName ."<br>";
+        echo "Specialty 1: ". $specialty1 ."<br>";
+        echo "Specialty 2: ". $specialty2 ."<br>";
+        echo "Specialty 3: ". $specialty3 ."<br>";
+        echo "E-Mail: ". $email ."<br>";
+        echo "Phone Number: ". $phoneNr ."<br>";
+        echo "Date of Birth: ". $bday ."<br>";
+
+    }
+    if ($fName && $lName && $specialty1 && $specialty2 && $specialty3 && $email && $phoneNr && $bday){
+        $dbHandler = new PDO("mysql:host=mysql;dbname=e3t_database;charset=utf8", "root", "qwerty");
+        try {
+            $sql= $dbHandler->prepare("INSERT INTO talents(`id`,`fName`,`lName`,`specialty1`,`specialty2`,
+                                                    `specialty3`,`email`,`phoneNr`,`bday`,`description`,`uploadedFile`)
+                                                    VALUES( NOT NULL,:fName,:lName,:specialty1,:specialty2,:specialty3,
+                                                           :email,:phoneNr,:bday,:description,:uploadedFile);");
+            $sql->bindParam("fName",$fName,PDO::PARAM_STR);
+            $sql->bindParam("lName",$lName,PDO::PARAM_STR);
+            $sql->bindParam("specialty1",$specialty1,PDO::PARAM_STR);
+            $sql->bindParam("specialty2",$specialty2,PDO::PARAM_STR);
+            $sql->bindParam("specialty3",$specialty3,PDO::PARAM_STR);
+            $sql->bindParam("email",$email,PDO::PARAM_STR);
+            $sql->bindParam("phoneNr",$phoneNr,PDO::PARAM_STR);
+            $sql->bindParam("bday",$bday,PDO::PARAM_STR);
+            $sql->bindParam("description",$description,PDO::PARAM_STR);
+            $sql->bindParam("uploadedFile",$uploadedFile,PDO::PARAM_STR);
+
+            $sql->execute();
+        }catch (Exception $ex){
+            print $ex;
+        }
+
+
+        //$dbHandler = NULL;
+       echo "<a href='newClient.php'> Add another client </a>";
+
+    }else{
+        echo "something";
+    }
+
+}
 ?>
