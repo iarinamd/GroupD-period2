4d3bf05fd0e33c607e13c8aaa681a31ce698e3cf
izabelarybinska <izabela.rybinska@student.nhlstenden.com>
2023-01-11 14:31:39 +0100

    finished new clients page

--- newClient/newClient.php
+++ newClient/newClient.php
@@ -106,7 +106,7 @@ try {
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/newClientStyle.css">
         <title>Create a new Client!</title>
     </head>
     <body>
