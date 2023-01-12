9f587be65a5e163b54244aeff2ec9f5fe651ab5d
Alex Schuepbach <alex.schuepbach@student.nhlstenden.com>
2023-01-12 15:23:07 +0100

    Included header and footer in home-page

--- footer.php
+++ footer.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>header and footer</title>
-    <meta name="viewpoint" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="footer.css">
-</head>
-<body>
+
     <footer>
         <div class="contactInformations">
            <h3> CONTACT</h3>
@@ -20,15 +12,13 @@
         </div>
         <div class="iconImg">
             <a href="https://nl-nl.facebook.com/">
-                <img class="facebookImg" src="image/facebook_logo.png" alt="facebook logo">
+                <img class="facebookImg" src="img/facebook_logo.png" alt="facebook logo">
             </a>
             <a href="https://www.instagram.com/">
-                <img class="instagramImg" src="image/instagram_logo.png" alt="instagram logo">
+                <img class="instagramImg" src="img/instagram_logo.png" alt="instagram logo">
             </a>
             <a href="https://www.tiktok.com/login">
-                <img class="tiktokImg" src="image/tiktok_logo.png" alt="tiktok logo">
+                <img class="tiktokImg" src="img/tiktok_logo.png" alt="tiktok logo">
             </a>
         </div>
     </footer>
-</body>
-</html>
--- header.php
+++ header.php
@@ -1,16 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>header and footer</title>
-    <meta name="viewpoint" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="header.css">
-</head>
-<body>
+
     <header>
         <div id ="headerImage">
             <a href="#">
-                <img class="logoImg" src="image/E3T_LOGO.png" alt="E3T logo">
+                <img class="logoImg" src="img/e3tLogo.png" alt="E3T logo">
             </a>
         </div>
         <nav>
@@ -21,5 +13,3 @@
             </ul>
         </nav>
     </header>
-</body>
-</html>
