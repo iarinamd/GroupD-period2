a50d298a313add8b154a670d559ce650223109a8
izabelarybinska <izabela.rybinska@student.nhlstenden.com>
2023-01-13 15:01:56 +0100

    relocated files

--- browseTalents.php
+++ browseTalents.php
@@ -14,24 +14,12 @@
     <title>Browse Talents</title>
 </head>
 <body>
-<header>
-    <img src="img/e3tLogo.png"
-         alt="E3T_logo">
-    <nav>
-        <ul>
-            <li><a href="browseTalents.php">BROWSE TALENTS</a></li>
-            <li><a href="browseCategories.php">EVENTS</a></li>
-            <?php
-            if(isset($_SESSION['login']) AND $_SESSION['login'] == 'loged'){
-                echo"<li><a href = 'li-profile.php'>PROFILE</a></li>";
-            }
-            else{
-                echo"<li><a href = 'login2.php'>LOGIN</a></li>";
-            }
-            ?>
-        </ul>
-    </nav>
-</header>
+    <header>
+        <?php
+            include 'header.php';
+        ?>
+    </header>
+
 
 <div class="mainContainer">
     <!-- Start of talent categories section
@@ -52,5 +40,10 @@
 
     </main>
 </div>
+<footer>
+    <?php
+        include 'footer.php';
+    ?>
+</footer>
 </body>
 </html>
