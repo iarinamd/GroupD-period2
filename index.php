2297ab33a40124a2b3a70e28adaba3f119aef46e
Alex Schuepbach <alex.schuepbach@student.nhlstenden.com>
2023-01-14 12:54:46 +0100

    Renamed home-page-style to indexStyle

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <title>Home page</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" type="text/css" href="css/home-page-style.css">
+        <link rel="stylesheet" type="text/css" href="css/indexStyle.css">
     </head>
 
     <body>
