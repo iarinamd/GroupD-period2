7840ee29287610c3a7815491f9e78229fd82ba7a
¨Alicia <Alicia.Rodriguez.benaches@student.nhlstenden.com>
2023-01-14 13:05:28 +0100

    changed an anchor

--- login2.php
+++ login2.php
@@ -37,6 +37,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){ // related to form, verify if the met
     while($result = $stmt->fetch(PDO::FETCH_ASSOC)){
         if($result["username"] == $_POST["userInput"] AND password_verify($_POST["passwordInput"], $result["password"])){
             $_SESSION["login"] = "loged";
+            echo "You have succesfuly loged in";
+            echo "<a href ='index.php'>Go to main page</a>";
         }
     }
 }
